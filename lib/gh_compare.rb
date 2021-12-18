a65848e400b5aef993cff76a1d9ef6fd1c41cc1a
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-12-18 20:44:47 +0900

    Add remote_url method

--- lib/gh_compare.rb
+++ lib/gh_compare.rb
@@ -2,5 +2,30 @@ require "gh_compare/version"
 
 module GhCompare
   class Error < StandardError; end
-  # Your code goes here...
+
+  class CommitCompare
+    attr_reader :remote_origin
+
+    def initialize
+      @remote_origin = get_remote_origin
+    end
+
+    def remote_url
+      remote_url? ? remote_origin.gsub(/\.git$/, '') : ssh_to_url
+    end
+
+    private
+
+    def get_remote_origin
+      `git config --get remote.origin.url`.strip
+    end
+
+    def remote_url?
+      URI.parse(remote_origin).is_a?(URI::HTTP) rescue false
+    end
+
+    def ssh_to_url
+      "https://github.com/#{remote_origin.gsub(/git@github.com:/, '').gsub(/\.git$/, '')}"
+    end
+  end
 end
--- spec/gh_compare_spec.rb
+++ spec/gh_compare_spec.rb
@@ -1,9 +1,33 @@
+require_relative '../lib/gh_compare'
+
 RSpec.describe GhCompare do
   it "has a version number" do
     expect(GhCompare::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'remote_url' do
+    context 'ssh' do
+      let(:commit_compare) { GhCompare::CommitCompare.new }
+
+      before 'ssh setting' do
+        allow(commit_compare).to receive(:remote_origin).and_return('git@github.com:kenzo-tanaka/test.git')
+      end
+
+      it 'return github url' do
+        expect(commit_compare.remote_url).to eq 'https://github.com/kenzo-tanaka/test'
+      end
+    end
+
+    context 'url' do
+      let(:commit_compare) { GhCompare::CommitCompare.new }
+
+      before 'remote url setting' do
+        allow(commit_compare).to receive(:remote_origin).and_return('https://github.com/kenzo-tanaka/test.git')
+      end
+
+      it 'return github url' do
+        expect(commit_compare.remote_url).to eq 'https://github.com/kenzo-tanaka/test'
+      end
+    end
   end
 end
