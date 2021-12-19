010ed375b1f4f6a9f51537d5abbc043a12fcb332
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-12-19 20:13:23 +0900

    Add feature to exec/commit_compare

--- lib/gh_compare/exec/commit_compare.rb
+++ lib/gh_compare/exec/commit_compare.rb
@@ -1,7 +1,54 @@
+require 'optparse'
+require_relative '../commit_compare'
+
 module GhCompare::Exec
   class CommitCompare
+    attr_reader :options
+
+    def initialize(args)
+      @options = parse_args(args)
+    end
+
     def run
-      'https://github.com/kenzo-tanaka/test/compare/6ac8a0a82033ef1cbaf2883dfd3aca6e6f77a36f...109d0bcc7fa26dcacbac8c6607fd0ca218346f96'
+      compare_before if num_arg_passed?
+      compare_url if commit_arg_passed?
+    end
+
+    private
+
+    def parse_args(args)
+      opts = {}
+      OptionParser.new do |opt|
+        opt.on('--n=NUM', Integer) { |v| opts[:num] = v }
+        opt.on('--d==V,V', Array) { |v| opts[:commits] = v }
+
+        opt.parse!(args)
+      end
+      opts
+    end
+
+    def num_arg_passed?
+      options[:num]
+    end
+
+    def compare_before
+      GhCompare::CommitCompare.new.compare_before(options[:num])
+    end
+
+    def commit_arg_passed?
+      options[:commits]
+    end
+
+    def compare_url
+      GhCompare::CommitCompare.new.compare_url(from_commit, to_commit)
+    end
+
+    def from_commit
+      options[:commits][0]
+    end
+
+    def to_commit
+      options[:commits][1]
     end
   end
 end
--- spec/gh_compare/exec/commit_compare_spec.rb
+++ spec/gh_compare/exec/commit_compare_spec.rb
@@ -1,11 +1,35 @@
 RSpec.describe GhCompare::Exec::CommitCompare do
   describe 'run' do
-    let(:executor) { GhCompare::Exec::CommitCompare.new }
+    context 'num option passed' do
+      let(:executor) { GhCompare::Exec::CommitCompare.new(%w[-n 3]) }
 
-    subject { executor.run }
+      before 'mock commit compare' do
+        commit_compare_mock = double('GhCompare::CommitCompare')
+        allow(commit_compare_mock).to receive(:compare_before).and_return('https://github.com/kenzo-tanaka/test/compare/6ac8a0a82033ef1cbaf2883dfd3aca6e6f77a36f...109d0bcc7fa26dcacbac8c6607fd0ca218346f96')
+        allow(executor).to receive(:compare_before).and_return(commit_compare_mock.compare_before)
+      end
 
-    it 'return compare url' do
-      expect(subject).to eq 'https://github.com/kenzo-tanaka/test/compare/6ac8a0a82033ef1cbaf2883dfd3aca6e6f77a36f...109d0bcc7fa26dcacbac8c6607fd0ca218346f96'
+      subject { executor.run }
+
+      it 'return compare url' do
+        expect(subject).to eq 'https://github.com/kenzo-tanaka/test/compare/6ac8a0a82033ef1cbaf2883dfd3aca6e6f77a36f...109d0bcc7fa26dcacbac8c6607fd0ca218346f96'
+      end
+    end
+
+    context 'commit hash passed' do
+      let(:executor) { GhCompare::Exec::CommitCompare.new(%w[-d 6ac8a0a82033ef1cbaf2883dfd3aca6e6f77a36f,109d0bcc7fa26dcacbac8c6607fd0ca218346f96]) }
+
+      before 'mock commit compare' do
+        commit_compare_mock = double('GhCompare::CommitCompare')
+        allow(commit_compare_mock).to receive(:compare_url).and_return('https://github.com/kenzo-tanaka/test/compare/6ac8a0a82033ef1cbaf2883dfd3aca6e6f77a36f...109d0bcc7fa26dcacbac8c6607fd0ca218346f96')
+        allow(executor).to receive(:compare_url).and_return(commit_compare_mock.compare_url)
+      end
+
+      subject { executor.run }
+
+      it 'return compare url' do
+        expect(subject).to eq 'https://github.com/kenzo-tanaka/test/compare/6ac8a0a82033ef1cbaf2883dfd3aca6e6f77a36f...109d0bcc7fa26dcacbac8c6607fd0ca218346f96'
+      end
     end
   end
 end
